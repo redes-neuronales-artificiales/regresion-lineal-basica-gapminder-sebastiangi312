7ec7eeb5bd0b08402ffd0b1b9cf3f36ec6fe8260
sebastiangi312 <40746232+sebastiangi312@users.noreply.github.com>
2022-05-17 23:53:46 -0500

    v

--- preguntas.py
+++ preguntas.py
@@ -52,16 +52,16 @@ def pregunta_02():
     print(df.shape)
 
     # Imprima la correlación entre las columnas `life` y `fertility` con 4 decimales.
-    print(round(df['life'].corr(df['fertility']),4))
+    print(df['life'].corr(df['fertility']).round(4))
 
     # Imprima la media de la columna `life` con 4 decimales.
-    print(round(df['life'].mean(),4))
+    print(df['life'].mean().round(4))
 
     # Imprima el tipo de dato de la columna `fertility`.
     print(type(df['fertility']))
 
     # Imprima la correlación entre las columnas `GDP` y `life` con 4 decimales.
-    print(round(df['GDP'].corr(df['life']),4))
+    print(df['GDP'].corr(df['life']).round(4))
 
 
 def pregunta_03():
@@ -111,36 +111,36 @@ def pregunta_04():
     # Importe LinearRegression
     # Importe train_test_split
     # Importe mean_squared_error
-    from ____ import ____
+    from sklearn.linear_model import LinearRegression, train_test_split, mean_squared_error
 
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = ____
+    df = pd.read_csv('gm_2008_region.csv')
 
     # Asigne a la variable los valores de la columna `fertility`
-    X_fertility = ____
+    X_fertility = df["fertility"]
 
     # Asigne a la variable los valores de la columna `life`
-    y_life = ____
+    y_life = df["life"]
 
     # Divida los datos de entrenamiento y prueba. La semilla del generador de números
     # aleatorios es 53. El tamaño de la muestra de entrenamiento es del 80%
-    (X_train, X_test, y_train, y_test,) = ____(
-        ____,
-        ____,
-        test_size=____,
-        random_state=____,
+    (X_train, X_test, y_train, y_test,) = train_test_split(
+        X_fertility,
+        y_life,
+        test_size=0.8,
+        random_state=53,
     )
 
     # Cree una instancia del modelo de regresión lineal
-    linearRegression = ____
+    linearRegression = LinearRegression(fit_intercept=True, normalize=False, copy_X=True)
 
     # Entrene el clasificador usando X_train y y_train
-    ____.fit(____, ____)
+    linearRegression.fit(X_fertility, y_life)
 
     # Pronostique y_test usando X_test
-    y_pred = ____
+    y_pred = linearRegression(X_test)
 
     # Compute and print R^2 and RMSE
     print("R^2: {:6.4f}".format(linearRegression.score(X_test, y_test)))
-    rmse = np.sqrt(____(____, ____))
+    rmse = mean_squared_error(y_life, y_pred, squared=False)
     print("Root Mean Squared Error: {:6.4f}".format(rmse))
