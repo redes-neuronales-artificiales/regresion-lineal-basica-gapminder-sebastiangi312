14945e7900258d677aec0bfb2151f7f97a916745
sebastiangi312 <40746232+sebastiangi312@users.noreply.github.com>
2022-06-09 21:36:39 -0500

    answer question 4

--- preguntas.py
+++ preguntas.py
@@ -115,10 +115,10 @@ def pregunta_04():
     df = pd.read_csv('gm_2008_region.csv')
 
     # Asigne a la variable los valores de la columna `fertility`
-    X_fertility = df["fertility"]
+    X_fertility = df["fertility"].values.reshape(-1, 1)
 
     # Asigne a la variable los valores de la columna `life`
-    y_life = df["life"]
+    y_life = df["life"].values.reshape(-1, 1)
 
     # Divida los datos de entrenamiento y prueba. La semilla del generador de números
     # aleatorios es 53. El tamaño de la muestra de entrenamiento es del 80%
@@ -133,12 +133,13 @@ def pregunta_04():
     linearRegression = LinearRegression(fit_intercept=True, normalize=False, copy_X=True)
 
     # Entrene el clasificador usando X_train y y_train
-    linearRegression.fit(X_fertility, y_life)
+    linearRegression.fit(X_train, y_train)
 
     # Pronostique y_test usando X_test
-    y_pred = linearRegression(X_test)
+    y_pred = linearRegression.predict(X_test)
 
     # Compute and print R^2 and RMSE
     print("R^2: {:6.4f}".format(linearRegression.score(X_test, y_test)))
-    rmse = mean_squared_error(y_life, y_pred, squared=False)
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
     print("Root Mean Squared Error: {:6.4f}".format(rmse))
+
